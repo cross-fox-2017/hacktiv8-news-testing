e655fb75d01836956e8aab3f93eb356086bc1d2d
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-06 11:15:20 +0700

    Init testing

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Menu } from './components'
+import { Routes } from './Routes'
+
+const App = () => (
+  <div className='App'>
+    <div className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <h6>Hacktiv8 News and Peoples</h6>
+    </div>
+    <Router>
+      <div>
+        <Menu />
+        <Routes />
+      </div>
+    </Router>
+  </div>
+)
+
+export default App
