1157c439c9acecc84a0c1cbc8543b546957e48ee
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-06 11:32:48 +0700

    Init testing ver 3

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,9 @@ import App from './App';
 import { shallow, mount } from 'enzyme'
 import { expect } from 'chai'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+})
